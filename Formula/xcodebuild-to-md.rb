eb6c2d7a651d4e9db13ffb58fc58a5a2fd6c5776
David House <davidahouse@gmail.com>
2019-12-10 16:23:31 -0500

    Update xcodebuild-to-md.rb

--- Formula/xcodebuild-to-md.rb
+++ Formula/xcodebuild-to-md.rb
@@ -4,8 +4,9 @@
 class XcodebuildToMd < Formula
   desc "XCResult converter to markdown"
   homepage "https://github.com/davidahouse/xcodebuild-to-md"
-  url "https://github.com/davidahouse/xcodebuild-to-md"
-  sha256 "152ef26501839d1d40d784506b6429bffd786cc2"
+  url "https://github.com/davidahouse/xcodebuild-to-md.git",
+      :tag      => "v0.1.0",
+      :revision => "152ef26501839d1d40d784506b6429bffd786cc2"
   head "https://github.com/davidahouse/xcodebuild-to-md.git"
 
   # depends_on "cmake" => :build
